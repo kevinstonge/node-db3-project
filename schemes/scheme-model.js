527b1ecf02187d7820be3d966bba28ba16a83283
Kevin StOnge <kevinstonge@gmail.com>
2020-11-17 11:33:32 -0500

    all model functions created, not all tested yet

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -5,8 +5,51 @@ const find = () => {
   return db("schemes");
 };
 
-const findById = (id) => {
-  return db("schemes").where({ id });
+const findById = async (id) => {
+  try {
+    const [result] = await db("schemes").where({ id });
+    return result ? result : Promise.resolve(null);
+  } catch (error) {
+    throw error;
+  }
 };
 
-module.exports = { find, findById };
+const findSteps = async (id) => {
+  try {
+    const result = await db("steps")
+      .where({ scheme_id: id })
+      .orderBy("step_number");
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const add = async (schemeObject) => {
+  try {
+    const result = await db("schemes").insert(schemeObject);
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const update = async (udateObject, id) => {
+  try {
+    const result = await db("schemes").update(updateObject).where({ id });
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const remove = async (id) => {
+  try {
+    const result = await db("schemes").where({ id }).del();
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = { find, findById, findSteps, add, update, remove };
