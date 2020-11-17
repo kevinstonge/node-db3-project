80fad550a7bd29200698d83629800db0d36ea9f9
Kevin StOnge <kevinstonge@gmail.com>
2020-11-17 08:59:21 -0500

    scheme-model in progress

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,1 +1,12 @@
 // scheme-model
+const db = require("../data/db-config.js");
+
+const find = () => {
+  return db("schemes");
+};
+
+const findById = (id) => {
+  return db("schemes").where({ id });
+};
+
+module.exports = { find, findById };
